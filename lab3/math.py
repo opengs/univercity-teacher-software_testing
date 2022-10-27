c2023db3839701d7098bb7c4bf57d68623a7355f
Mykola Zhyhallo <mzhyhall@gmail.com>
2022-10-27 16:47:38 +0200

    Added typings for lab3

--- lab3/math.py
+++ lab3/math.py
@@ -1,22 +1,21 @@
+from typing import Optional
 
-# General math
-
-def max(*args):
+def max(*args: int) -> Optional[int]:
     '''Returns maximum value or None if there are no arguments'''
     raise NotImplementedError()
 
-def min(*args):
+def min(*args: int) -> Optional[int]:
     '''Returns minimum value or None if there are no arguments'''
     raise NotImplementedError()
 
-def sign(val):
+def sign(val: int) -> Optional[int]:
     '''Returns -1 if lavue is less then 0, 1 if value is greater than 0, 0 if value is zero'''
     raise NotImplementedError()
 
-def avg(*args):
+def avg(*args: int) -> Optional[int]:
     '''Returns average value of the list of value'''
     raise NotImplementedError()
 
-def median(*args):
+def median(*args: int) -> Optional[int]:
     '''Returns the median of the unsorted list of values'''
     raise NotImplementedError()
